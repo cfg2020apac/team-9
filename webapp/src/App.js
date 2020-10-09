9109c64785154fb1cd215b425f86dcbef6471a83
Cre8tion <ljl1997@gmail.com>
2020-10-09 22:55:00 +0800

    A

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,6 +4,37 @@ import './App.css';
 
 
 function App() {
+
+  const site = window.location.origin
+  const auth = `https://zoom.us/oauth/authorize?response_type=code&redirect_uri=${site}&client_id=LFnBwJLsSRKhafeNUdYiVw`
+  
+
+  function a(){
+    let params = new URLSearchParams(document.location.search.substring(1))
+    let code = params.get("code")
+
+    const getaccesstoken = `https://zoom.us/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${site}`
+    
+    var buffer = new Buffer("Basic LFnBwJLsSRKhafeNUdYiVw:MYmxuf7ogBMta0RiXam6bFFPflSCT6A9");
+    let cred = buffer.toString('base64');
+
+
+
+    /*
+    fetch(getaccesstoken, {
+        method: 'POST',
+        headers:{
+          'Authorization': cred,
+          'mode':'cors',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+        }
+      }
+    ).then(function(res){ return res })
+    .then(function(data){ console.log( data ) })
+    */
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,8 +50,11 @@ function App() {
         >
           Learn React
         </a>
-        <a href="https://zoom.us/oauth/authorize?response_type=code&redirect_uri=https://marketplace.zoom.us/docs/oauth/callback/success&client_id=LFnBwJLsSRKhafeNUdYiVw">
-          asdas
+        <a href={auth}>
+          auth
+        </a>
+        <a onClick={a}>
+          aaa
         </a>
       </header>
     </div>
